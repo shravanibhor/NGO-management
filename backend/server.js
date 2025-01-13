3ddf82c52638abd4c93be8fefd072dbff7d70e26
Shravani <shravanibhor3084@gmail.com>
2025-01-13 12:53:41 +0530

    server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/admin', require('./routes/adminRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
